51e7ae02ad01c47994ccd15b214bd9486de523c0
leemited <tecooni@naver.com>
2018-10-05 15:59:24 +0900

    first commit

---
+++ data/cache/latest-givemonth-basic-1-40.php
@@ -0,0 +1,83 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$bo_subject='기부천사';
+$list=array (
+  0 => 
+  array (
+    'wr_id' => '2',
+    'wr_num' => '-2',
+    'wr_reply' => '',
+    'wr_parent' => '0',
+    'wr_is_comment' => '0',
+    'wr_comment' => '0',
+    'wr_comment_reply' => '',
+    'ca_name' => '',
+    'wr_option' => '',
+    'wr_subject' => '플러그미디어웍스',
+    'wr_content' => '디자인회사(청주, 용정동)',
+    'wr_link1' => '',
+    'wr_link2' => '',
+    'wr_link1_hit' => '0',
+    'wr_link2_hit' => '0',
+    'wr_hit' => '5',
+    'wr_good' => '0',
+    'wr_nogood' => '0',
+    'mb_id' => 'admin',
+    'wr_password' => '',
+    'wr_name' => '최고관리자',
+    'wr_email' => 'admin@domain.com',
+    'wr_homepage' => '',
+    'wr_datetime' => '2018-06-28 14:03:44',
+    'wr_file' => '0',
+    'wr_last' => '2018-06-28 14:03:44',
+    'wr_ip' => '61.85.212.250',
+    'wr_facebook_user' => '',
+    'wr_twitter_user' => '',
+    'wr_1' => '',
+    'wr_2' => '',
+    'wr_3' => '',
+    'wr_4' => '',
+    'wr_5' => '',
+    'wr_6' => '',
+    'wr_7' => '',
+    'wr_8' => '',
+    'wr_9' => '',
+    'wr_10' => '',
+    'is_notice' => false,
+    'subject' => '플러그미디어웍스',
+    'comment_cnt' => '',
+    'datetime' => '2018-06-28',
+    'datetime2' => '06-28',
+    'last' => '2018-06-28',
+    'last2' => '06-28',
+    'name' => '<span class="sv_member">최고관리자</span>',
+    'reply' => 0,
+    'icon_reply' => '',
+    'icon_link' => '',
+    'ca_name_href' => 'http://fluggiveyou.cafe24.com/bbs/board.php?bo_table=givemonth&amp;sca=',
+    'href' => 'http://fluggiveyou.cafe24.com/bbs/board.php?bo_table=givemonth&amp;wr_id=2&amp;',
+    'comment_href' => 'http://fluggiveyou.cafe24.com/bbs/board.php?bo_table=givemonth&amp;wr_id=2&amp;',
+    'icon_new' => '',
+    'icon_hot' => '',
+    'icon_secret' => '',
+    'link' => 
+    array (
+      1 => NULL,
+      2 => NULL,
+    ),
+    'link_href' => 
+    array (
+      1 => 'http://fluggiveyou.cafe24.com/bbs/link.php?bo_table=givemonth&amp;wr_id=2&amp;no=1',
+      2 => 'http://fluggiveyou.cafe24.com/bbs/link.php?bo_table=givemonth&amp;wr_id=2&amp;no=2',
+    ),
+    'link_hit' => 
+    array (
+      1 => 0,
+      2 => 0,
+    ),
+    'file' => 
+    array (
+      'count' => '0',
+    ),
+  ),
+)?>
